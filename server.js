2a0069af3ed1dda497914bc022d0ca9f34213374
pjoon357 <pjoon357@naver.com>
2021-07-22 16:47:41 +0900

    feat: add Babel

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
+import {
+    ApolloServerPluginLandingPageGraphQLPlayground
+} from "apollo-server-core";
 
 const typeDefs = gql`
     type Query{
